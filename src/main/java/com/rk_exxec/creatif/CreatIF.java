9b9cf0452cf85f888b3c9182297746ec4ee90b80
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2026-09-19 23:33:25 +0200

    tooltip text

--- src/main/java/com/rk_exxec/creatif/CreatIF.java
+++ src/main/java/com/rk_exxec/creatif/CreatIF.java
@@ -1,7 +1,10 @@
 package com.rk_exxec.creatif;
 
 import com.mojang.logging.LogUtils;
+
+import net.createmod.catnip.lang.FontHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -23,9 +26,16 @@ import org.slf4j.Logger;
 import com.rk_exxec.creatif.filter.IngredientFilterMenu;
 import com.rk_exxec.creatif.filter.IngredientFilterScreen;
 import com.rk_exxec.creatif.network.IngredientFilterScreenPacket;
+import com.rk_exxec.creatif.util.MyGuiTextures;
+import com.rk_exxec.creatif.util.MyItems;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipModifier;
+import com.tterrag.registrate.util.entry.ItemEntry;
 import com.rk_exxec.creatif.filter.IngredientFilterItem;
 
-
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.MenuScreens.ScreenConstructor;
@@ -38,9 +48,11 @@ import net.minecraftforge.network.NetworkEvent.Context;
 
 import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
 
+import java.util.Properties;
+
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(CreateIngredientFilter.MODID)
-public class CreateIngredientFilter
+@Mod(CreatIF.MODID)
+public class CreatIF
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "creatif";
@@ -50,11 +62,20 @@ public class CreateIngredientFilter
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final RegistryObject<IngredientFilterItem> INGREDIENT_FILTER_ITEM = ITEMS.register("ingredient_filter",
-        () -> new IngredientFilterItem(new Item.Properties().stacksTo(1)));
+    // public static final RegistryObject<IngredientFilterItem> INGREDIENT_FILTER_ITEM = ITEMS.register("ingredient_filter",
+        // () -> new IngredientFilterItem(new Item.Properties().stacksTo(1)));
+
+    
+
+    private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID)
+		.defaultCreativeTab((ResourceKey<CreativeModeTab>) null)
+		.setTooltipModifierFactory(item ->
+			new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE)
+				.andThen(TooltipModifier.mapNull(KineticStats.create(item)))
+		);
 
     public static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(ForgeRegistries.MENU_TYPES, MODID);
-    public static final RegistryObject<MenuType<IngredientFilterMenu>> INGREDIENT_FILTER_MENU = MENU_TYPES.register("ingredient_filter", CreateIngredientFilter::createIngredientFilterMenu);
+    public static final RegistryObject<MenuType<IngredientFilterMenu>> INGREDIENT_FILTER_MENU = MENU_TYPES.register("ingredient_filter", CreatIF::createIngredientFilterMenu);
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
         ResourceLocation.fromNamespaceAndPath(MODID, "main"),
         () -> "1",
@@ -73,17 +94,21 @@ public class CreateIngredientFilter
         return IForgeMenuType.create((id, inventory, buffer) -> new IngredientFilterMenu(INGREDIENT_FILTER_MENU.get(), id, inventory, buffer));
     }
 
-    public CreateIngredientFilter(FMLJavaModLoadingContext context)
+    public CreatIF(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        REGISTRATE.registerEventListeners(modEventBus);
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
+        // ITEMS.register(modEventBus);
+
+        MyItems.register();
+        // () -> new IngredientFilterItem(new Item.Properties().stacksTo(1)))
 
         MENU_TYPES.register(modEventBus);
         // CHANNEL.registerMessage(0, SetMatchAnyPacket.class, SetMatchAnyPacket::encode, SetMatchAnyPacket::decode, SetMatchAnyPacket::handle);
@@ -120,7 +145,7 @@ public class CreateIngredientFilter
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
         if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES)
-            event.accept(INGREDIENT_FILTER_ITEM);
+            event.accept(MyItems.INGREDIENT_FILTER_ITEM.asItem());
     }
 
 
@@ -143,5 +168,9 @@ public class CreateIngredientFilter
         {
             MenuScreens.register((MenuType) INGREDIENT_FILTER_MENU.get(), (ScreenConstructor) (menu, inventory, title) -> new IngredientFilterScreen((IngredientFilterMenu) menu, inventory, title));
         }
+    }
+
+    public static CreateRegistrate getRegistrate(){
+        return REGISTRATE;
     }
 }
